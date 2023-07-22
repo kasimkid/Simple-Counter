d0dc10e8a0c9df920261024a56644934f636a84a
Rafael Araujo <kasim.85.ra@gmail.com>
2023-07-22 19:45:51 -0400

    estilos counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenedor">
+		<div className="timer">
+			<div className="box1">{props.counter1}</div>
+			<div className="box2">{props.counter2}</div>
+			<div className="box3">{props.counter3}</div>
+
+			<div className="box4">{props.counter4}</div>
+			<div className="box5">{props.counter5}</div>
+			<div className="box6">{props.counter6}</div>
+			<div className="reloj"><i class="far fa-clock"></i></div>
+	
 		</div>
+		</div>
+		
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,59 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter1 = 0;
+let counter2 = 0;
+let counter3 = 0;
+let counter4 = 0;
+let counter5 = 0;
+let counter6 = 0;
+
+// let rest1 = 0;
+// let rest2 = 0;
+// let rest3 = 0;
+// let rest4 = 0;
+// let rest5 = 0;
+// let rest6 = 0;
+
+
+
+
+// rest1 = Math.floor(counter1 / 10)
+// rest2 = Math.floor(counter2 / 100)
+// rest3 = Math.floor(counter3 / 1000)
+setInterval(() => {
+    counter1 +=1  
+if(counter1 > 9 ){
+    counter2 +=1
+    counter1 = 0
+    
+
+}
+if (counter2 > 9){
+    counter3 +=1
+    counter2 = 0
+}
+if (counter3 > 9){
+    counter4 +=1
+    counter3 = 0
+}
+if (counter4> 9){
+    counter5 +=1
+    counter4 = 0
+}
+if (counter5 > 9){
+    counter6 +=1
+    counter5 = 0
+}
+
+
+
+    ReactDOM.render(<Home counter1 = {counter1} counter2 = {counter2} counter3 = {counter3} counter4 = {counter4} counter5 = {counter5} counter6 = {counter6} />, 
+    document.querySelector("#app"));
+
+   
+}, 100);
+
+
+
+
